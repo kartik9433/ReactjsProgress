1c95b994026019395fb813b5671ce42f1bff2c31
kartik9433 <kartiksingh8923@gmail.com>
2026-04-06 01:13:59 +0530

    function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Blog,{TimeTable, userkey} from './Blog'
+// import Blog,{TimeTable, userkey} from './Blog'
+import  {User} from './User'
 
 function App() {
   return (
     <div>
-        <Blog/>
-       <TimeTable/>
-       <h1>{userkey}</h1>
+        <User/>
+        <Navbar/>
     </div>
+  )
+}
+
+function Navbar(){
+  return (
+    <h2>Navbar</h2>
   )
 }
 
---
+++ src/User.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function User() {
+  return (
+    <div>
+       <h1>This is our website</h1>
+    </div>
+  )
+}
+

b5cf9cedc51fb88c97d959d4c1d186079e5809de
kartik9433 <kartiksingh8923@gmail.com>
2026-04-07 18:01:07 +0530

    life cycle method

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   constructor(){
-    super();
+       super();
+    this.state = {
+       name:"Hello Sir",
+       count:0
+    };
     console.log("constructor");
   }
-   componentDidMount(){
+   componentDidUpdate(){
     console.log("component did Mount");
    }
    
@@ -14,6 +18,10 @@ export default class App extends Component {
     return (
       <div className="container">
          <h1>LifeCycleMethodComponetDidUpdate</h1>
+         <h1>count:{this.state.count}</h1>
+         <button onClick={()=>this.setState({count:this.state.count+1})}>count</button>
+         <h2>{this.state.name}</h2>
+         <button onClick={()=>this.setState({name :"kartik"})}>Click</button>
       </div>
     )
   }

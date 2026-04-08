b4bc7506e37d4f05a5665cc101caa350188aeac9
kartik9433 <kartiksingh8923@gmail.com>
2026-04-08 19:19:41 +0530

    Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
-import React from 'react'
-import Table from 'react-bootstrap/Table';
-
+import React,{Fragment} from 'react'
 
 function App() {
-  const tableData  = [
-    {id:1,name:"xyz",age:40,location:"Time Square"},
-    {id:2,name:"yyy",age:50,location:"Time Square"},
-    {id:3,name:"kmf",age:35,location:"Time Square"},
-    {id:4,name:"xdfz",age:70,location:"Time Square"}
-  ];
-  return (
-    <div>
-      <h1>Table</h1>
-        <Table>
-            <thead>
-                 <tr>
-                     <th>Id</th>
-                     <th>Name</th>
-                     <th>Age</th>
-                     <th>Location</th>
-                 </tr>
-            </thead>
-           <tbody>
-               {tableData.map((data,idx)=>(
-                  <tr key={idx}>
-                       <th>{data.id}</th>
-                       <th>{data.name}</th>
-                       <th>{data.age}</th>
-                       <th>{data.location}</th>
-                  </tr>
-               ))}
-           </tbody>
-        </Table>
-        
-    </div>
-  )
+return(    
+     <Fragment>
+       <h1>hello</h1>
+       <h1>hello</h1>
+     </Fragment>
+     )
 }
 
 export default App

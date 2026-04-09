4bb672ab9d2e840b602d2af58fa25c7085c1cbf7
kartik9433 <kartiksingh8923@gmail.com>
2026-04-09 21:37:22 +0530

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import React, { useState,useMemo } from 'react'
+import { useRef } from "react"
+import React from 'react'
 
 function App() {
-  const [number1,setNumber1] = useState(0);
-  const[number2,setNumber2] = useState(0);
-  const[count,setCount] = useState(0);
+  const inputRef = useRef(null);
 
-  const sum  = useMemo(()=>{
-    console.log("calculating sum...")
-    return number1+number2;
-  },[number1,number2])
-
+  const handleChange = ()=>{
+    const inputvalue  = inputRef.current.value;
+    alert(`input value  ${inputvalue}`);
+  }
   return (
     <div>
-      <h1>UseMemo</h1>
-       <input type="text" value={number1} onChange={(e)=>setNumber1(number1+1)} />
-       <input type="text" value={number2} onChange={(e)=>setNumber2(number2+1)} />
-            <h1>sum:{sum}</h1>
-       <button onClick={()=>setCount(count+1)}>Count++</button>
-        <h2>Count:{count}</h2>
+        <h1>useRef</h1>
+        <input type="text" ref={inputRef}/>
+        <button onClick={handleChange}>Get input ref</button>
     </div>
   )
 }
